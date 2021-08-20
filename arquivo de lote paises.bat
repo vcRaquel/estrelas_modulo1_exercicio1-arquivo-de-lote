3ffd69d6ed3fee6ddfea28445116298522565374
Raquel Leão <vc.raquel@gmail.com>
2021-08-20 15:29:47 -0300

    diretorios paises da america do sul

--- arquivo de lote paises.bat
+++ arquivo de lote paises.bat
@@ -1,1 +1,3 @@
 md "todos os paises"
+cd "todos os paises"
+md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela" 
