b4872e65e7721b2fef88f2e02814497381866573
Raquel Leão <vc.raquel@gmail.com>
2021-08-20 15:38:41 -0300

    diretorios estados do brasil

--- arquivo de lote paises.bat
+++ arquivo de lote paises.bat
@@ -1,3 +1,5 @@
 md "todos os paises"
 cd "todos os paises"
 md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela" 
+cd "Brasil"
+md "Goias" "Mato Grosso" "Mato Grosso do Sul" "Distrito Federal" "Alagoas" "Bahia" "Ceara" "Maranhao" "Paraiba" "Pernambuco" "Piaui" "Rio Grande do Norte" "Sergipe" "Acre" "Amapa" Amazonas" "Para" "Rondonia" "Roraima" "Tocantins" "Parana" "Santa Catarina" "Rio Grande do Sul" "Sao Paulo" "Rio de Janeiro" "Espirito Santo" "Minas Gerais"
