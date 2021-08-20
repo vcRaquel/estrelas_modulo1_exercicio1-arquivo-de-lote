f7bd2af19a00f4e7712b45f3f16f15e05f4a33eb
Raquel Leão <vc.raquel@gmail.com>
2021-08-20 14:59:05 -0300

    diretorios estados centro oeste

--- arquivo de lote estados.bat
+++ arquivo de lote estados.bat
@@ -1,3 +1,9 @@
 md "todos os estados"
 cd "todos os estados"
 md "centro oeste" "nordeste" "norte" "sul" "sudeste"
+cd "centro oeste"
+md "Goias"
+md "Mato Grosso"
+md "Mato Grosso do Sul"
+md "Distrito Federal"
+
